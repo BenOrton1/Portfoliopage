90294ae0a5d5d06e554c74fa4b51cddd71f3d516
Benjamin Orton <b.r.orton@gmail.com>
2019-11-19 09:48:34 +0000

    javasctip added and portfolio updated

--- static/script/script.js
+++ static/script/script.js
@@ -1,15 +1,14 @@
 $(document).ready(function () {
-    $("#portfolio").hide(1000);
     $("#about-me").hide(1000);
     $(".portfolio-button").click(function () {
-        console.log("button pressed")
-        $("#home").hide(1000);
-        $("#about-me").hide(1000);
-        $("#portfolio").show(1000);
+        console.log("button pressed");
+        $("#home").addClass("hide");
+        $("#portfolio").removeClass("hide");
+        
     });
     $(".about-button").click(function () {
         console.log("button pressed")
-        $("#home").hide(1000);
+        $("#home").addClass("hide");
         $("#portfolio").hide(1000);
         $("#about-me").show(1000);
     });
